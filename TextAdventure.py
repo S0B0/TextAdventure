39747f7fee4a47f806d0d47ec07e580a9675c024
0xS0B0 <43358340+S0B0@users.noreply.github.com>
2022-05-10 00:19:40 +0300

    updated riddles
    riddles now work + some more ascii art and logic magic

--- TextAdventure.py
+++ TextAdventure.py
@@ -48,7 +48,36 @@ def rulepage():
     print("   <=----------------------[ RULES ]-----------------------=>             ")
     
  
- 
+def drawtree():
+    typewriter("                         \n\
+           &&& &&  & &&             \n\
+      && &\/&\|& ()|/ @, &&         \n\
+      &\/(/&/&||/& /_/)_&/_&        \n\
+   &() &\/&|()|/&\/ '% #(())        \n\
+  &_\_&&_\ |& |&&/&__%_/_& &&       \n\
+&&   && & &| &| /& & % ()& /&&      \n\
+ ()&_---()&\&\|&&-&&--%---()~       \n\
+     &&     \|||                    \n\
+             |||                    \n\
+             |||                    \n\
+             |||                    \n\
+         -=-~  .-^- _             ",0.006,0.06)
+    
+def drawowl():
+    typewriter("\n\
+,_.                                                          ,_. \n\
+'\cXX.==- __                                        __ -==,XXv/` \n\
+    ~=_X7~ ,/~!g=-,_.                      ,_.-=s!~L. ~TX_=~     \n\
+       ~=c. = /- T--e'T|=v._  ....   _,v=!7`z--\ -\ = ,v=~       \n\
+          ~=c` ./ ,-`,/ /i/Z\/~~~~\/Z\i\ \.'-. \, 'v=~           \n\
+             ~\s,/ ,/ ,/ Y]`/ @/\@ \'[Y \. \. \.g/~              \n\
+                '`Yc.v`,/Vs)-  \/  -(sV\.'c,v+'`                 \n\
+                     ~~]mZczTV '` VTevZm[~~                      \n\
+                  ,=-T|--2Y [      ] Y2--!T-=.                   \n\
+                  'i`_ -|-'i!      !i`-!- _'i`                   \n\
+                    '-s|.cf ,!]\/[!. 1v,!g-`                     \n\
+                        ~Y/v/vv..vv\v\Y~                         \n\
+                         ^            ^",0.006,0.06)   
  
 def typewriter(message,s1,s2):
     for char in message:
@@ -65,7 +94,7 @@ def clear():
 
 
 def drawline():
-    print(" <=============-~oOxOo~-=============>")
+    typewriter(" \n<=============-~oOxOo~-=============>\n",0.01,0.1)
     
 
 def save():
@@ -75,11 +104,38 @@ def save():
         f.write(item + "\n")
     f.close()
  
-def riddles():
-     print("RIDDLES")
+def riddles(): 
+    tries = 3
+    while tries !=0:
+        drawline()
+        typewriter("spends all life in but a cage,\n\
+brings us joy yet still can rage \n\
+behold thee,the restless sage!\n\
+    \nwhat am I?",0.03,0.2)
+        drawline()
+        answer = input("\n>: ")
+        if answer == "heart":
+            typewriter("(rabbit): -hmmhmm....such a clever traveler...\n",0.03,0.2)
+            input("|enter|")
+            break
+        else:
+            if tries == 3:
+                typewriter("(rabbit): -I will give you two more tries..\n",0.03,0.2)
+                tries -= 1
+            elif tries == 2:
+                typewriter("(rabbit): -I will give you one more try..\n",0.03,0.2)
+                tries -= 1
+            elif tries == 1:
+                typewriter("(rabbit): -You failed to answer pilgrim, now your soul is mine! \n",0.03,0.2)   
+                input("\n|enter|")
+                tries = 0
+                break    
+      
+               
  
- 
+ # MEET THE RABBIT FROM IGNORE TREE/APPROACH TREE
 def rabbit(met_owl):
+    clear()
     typewriter("\n\
    \        \n\
     \ /\    \n\
@@ -92,24 +148,77 @@ Answer minth riddles and I shall grant you entrance. \n [riddles] \n [talk]",0.0
     op = input("\n>: ")
     if op == "riddles":
         riddles()
-    elif op == "ask" and met_owl == False:
+    elif op == "talk" and met_owl == False:
         typewriter(f"\n({name}): - Who are you? \n")
         typewriter("(Rabbit): - My name is of no importance. Know this : \n\
 I am but the guardian of this temple",0.03,0.2)
         print("[riddles]")
         riddles()
-    elif op == "ask" and met_owl == True:
+    elif op == "talk" and met_owl == True:
         typewriter(f"\n({name}): - I don't trust you. You have no heart. \n",0.03,0.2)
         input("\n|enter|")
         
     else:
         print("\nINVALID INPUT")  
         input("\n|enter|")  
+        rabbit()
     
+ # MEET THE OWL ROUTE
+def owl():
+        typewriter("\nYou see a great owl approaching...\n\
+You feel protected,this entity feels familiar.",0.03,0.2)
+        drawowl()
+        print("\n[greet] \n[nothing]")
+        dec = input("\n>: ")
+        if dec == "greet" or dec == "nothing":
+          
+            typewriter("\nTraveler,a warning : beware the one who speaks in riddles\n\
+for it has no H E A R T...",0.03,0.2)
+            met_owl = True
+            input("\n|enter|")
+         
+            typewriter("\nBefore you can say anything the great owl flies over you and fades\n\
+in the distance. This interaction was weird.You suddenly wake up. You feel refreshed",0.03,0.2)
+            input("\n|enter|")
+            clear()
+           
+            typewriter("You resume your journey. \n\
+The sun is slowly setting and it's piercing rays are less heavier.\n\
+Your eyes begin to reflect the first blinks of the stars.\n\
+Upon a rocky structure there lies a temple.",0.03,0.2)
+            input("\n|enter|")
+            
+            typewriter("...As you approach the temple you see a rabbit guarding the entrance.\n [approach]",0.03,0.2)
+            op = input("\n>: ")
+            if op == "approach":
+                rabbit(met_owl)
+        else:
+            print("invalid input! try again!")
+            input("\n|enter|")
+ 
+ # IGNORE TREE ROUTE
+def ignore_tree(met_owl):
+     clear()
+       
+     typewriter("You are devouted to the road. \n\
+The sun is blazing the land,it's rays feel heavy.\n\
+Despite the harsh environment you keep going.\n\
+Upon a rocky structure there lies a temple,weirdly your heart skips a beat.",0.03,0.2)
+     input("\n|enter|")
+        
+     typewriter("...As you approach the temple you see a rabbit guarding the entrance.\n [talk]",0.03,0.2)
+     op = input("\n>: ")
+     if op == "talk":
+            rabbit(met_owl)
+     else:
+            print("INVALID INPUT TRY AGAIN!")
+            ignore_tree()
+ 
  
 def prologue():
     met_owl = False
     clear()
+   
     typewriter("it's 1990 and you sit in your bedroom \n\
 playing a text adventure game on your computer.\n\
 The rain drops on your window reflect the pale shade of the full moon.\n\
@@ -117,11 +226,12 @@ you feel at ease...\n\
 As you begin your journey...",0.03,0.2)
     input("\n|enter|")
     clear()
+    
     typewriter("...",0.03,0.2)
     typewriter("You walk the path of the pilgrim,in search of truth.\n\
 The unforgiving sun is ever watching,your feet are heavy and your lips dry.\n\
-You approach a lonely tree in this vast desert . It's shade is tempting. \n\
-What do you do? ",0.03,0.2)
+You observe a lonely tree in this vast desert . It's shade is tempting. \n\ ",0.03,0.2)
+    drawtree()
     print("\n[reach tree]\n[ignore tree]")
     dir = input(">: ")
     
@@ -132,46 +242,17 @@ and a sudden breeze lifts the weight of your travel.\n\
 As you sit down,you begin to drift in and out of conciousness.\n\
 This tree seems special.",0.03,0.2)
         input("\n|enter|")
+       
         typewriter("\nYour mind falls into a deep slumber...\n\
 you start to dream of lush lands,the sky is clear here and the sound\n\
 of a shy river can be heard.",0.03,0.2)
         input("\n|enter|")
-        typewriter("\nYou see a great owl approaching...\n\
-You feel protected,this entity feels familiar.\n[greet owl] \n[remain silent]",0.03,0.2)
-        dec = input("\n>: ")
-        if dec == "greet owl" or dec == "remain silent":
-            typewriter("\nTraveler,a warning : beware the one who speaks in riddles\n\
-for it has no H E A R T...",0.03,0.2)
-            met_owl = True
-            input("\n|enter|")
-            typewriter("\nBefore you can say anything the great owl flies over you and fades\n\
-in the distance. This interaction was weird.You suddenly wake up. You feel refreshed",0.03,0.2)
-            input("\n|enter|")
-            clear()
-            typewriter("You resume your journey. \n\
-The sun is slowly setting and it's piercing rays are less heavier.\n\
-Your eyes begin to reflect the first blinks of the stars.\n\
-Upon a rocky structure there lies a temple.",0.03,0.2)
-            input("\n|enter|")
-            typewriter("...As you approach the temple you see a rabbit guarding the entrance.\n [talk]",0.03,0.2)
-            op = input("\n>: ")
-            if op == "talk":
-                rabbit(met_owl)
-        else:
-            print("invalid input! try again!")
-            input("\n|enter|")
+      
+        owl()       # O W L
            
     elif dir == "ignore tree":
-        clear()
-        typewriter("You are devouted to the road. \n\
-The sun is blazing the land,it's rays feel heavy.\n\
-Despite the harsh environment you keep going.\n\
-Upon a rocky structure there lies a temple,weirdly your heart skips a beat.",0.03,0.2)
-        input("\n|enter|")
-        typewriter("...As you approach the temple you see a rabbit guarding the entrance.\n [talk]",0.03,0.2)
-        op = input("\n>: ")
-        if op == "talk":
-            rabbit(met_owl)
+        ignore_tree(met_owl)
+            
     else:
         
         print("INVALID INPUT! TRY AGAIN!")
@@ -254,7 +335,5 @@ while run:
             menu = True
             save()
         elif option == "1":
-            prologue()
-
-
-
+           riddles()
+           # prologue()
